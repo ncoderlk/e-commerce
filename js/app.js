d6422876d4c49af5565e545b1b3105d3d64843ef
ncoderlk <ncoderlk@gmail.com>
2022-09-26 17:08:18 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ setInterval(() => {
   let date = new Date().getFullYear()
   document.getElementById('copyright').innerHTML = `&copy;${date} All rights reserved by Sithika Dangampala`
 }, 2000)
+function scrolltp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+let linksSet = document.querySelectorAll('a[href^="#"]');
+linksSet.forEach((link) => {
+  link.onclick = function (i) {
+    i.preventDefault();
+
+    let href = document.querySelector(this.hash);
+    href.scrollIntoView({
+      behavior: "smooth",
+      duration: "3s",
+    });
+  };
+});
